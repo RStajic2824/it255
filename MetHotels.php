441871def7a4df3dff54e37143b8b7a8ce6e5fa9
Rastislav Stajic <rastislav.stajic@metropolitan.ac.rs>
2017-05-28 18:31:38 +0200

    Potvrdi met hotele

---
+++ MetHotels.php
@@ -0,0 +1,6 @@
+<?php
+	echo "<div>
+		<p>Hotel Moskva</p>
+		<iframe src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2830.419014978008!2d20.458241815210517!3d44.81302767909864!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a7ab209cff051%3A0x52504f6400d4b777!2sHotel+%22Moscow%22!5e0!3m2!1ssr!2srs!4v1495985744282\" width=\"600\" height=\"450\" frameborder=\"0\" style=\"border:0\" allowfullscreen></iframe>
+	</div>";
+?>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 	echo "Hello world!";
+	// git commit url https://github.com/RStajic2824/it255.git
 ?>
